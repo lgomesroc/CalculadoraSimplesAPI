075ea49408c3f098d55318fdd361502a7bdbda53
Luciano Gomes Rocha <]'lgomesroc@hotmail.com>
2023-11-22 09:43:41 -0300

    Programa de calculadora em API

---
+++ Controllers/CalculadoraController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CalculadoraSimplesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculadoraSimplesAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CalculadoraController : Controller
+    {
+        [HttpGet("Somar")]
+        public ActionResult<Calculadora> Somar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.somar();
+            return Ok(calc);
+        }
+
+        [HttpGet("Subtrair")]
+        public ActionResult<Calculadora> Subtrair(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.subtrair();
+            return Ok(calc);
+        }
+
+        [HttpGet("Multiplicar")]
+        public ActionResult<Calculadora> Multiplicar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.multiplicar();
+            return Ok(calc);
+        }
+
+        [HttpGet("Dividir")]
+        public ActionResult<Calculadora> Dividir(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.dividir();
+            return Ok(calc);
+        }
+
+        [HttpGet("Potencializar")]
+        public ActionResult<Calculadora> Potencializar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.potencializar();
+            return Ok(calc);
+        }
+
+        [HttpGet("Radicionalizar")]
+        public ActionResult<Calculadora> Radicionalizar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.radicionalizar();
+            return Ok(calc);
+        }
+
+        [HttpGet("Percentualizar")]
+        public ActionResult<Calculadora> Percentualizar(double valor1, double valor2)
+        {
+            var calc = new Calculadora(valor1, valor2);
+            calc.percentualizar();
+            return Ok(calc);
+        }
+    }
+}
---
+++ Models/Calculadora.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace CalculadoraSimplesAPI.Models
+{
+    public class Calculadora
+    {
+        public double Valor1 { get; set; }
+        public double Valor2 { get; set; }
+        public double Resultado { get; set; }
+
+        public Calculadora() { }
+
+        public Calculadora(double valor1, double valor2)
+        {
+            this.Valor1 = valor1;
+            this.Valor2 = valor2;
+        }
+
+        public void somar()
+        {
+            Resultado = Valor1 + Valor2;
+        }
+
+        public void subtrair() 
+        {
+            Resultado = Valor1 - Valor2;
+        }
+        
+        public void multiplicar()
+        {
+            Resultado = Valor1 * Valor2;
+        }
+
+        public void dividir() 
+        {
+            Resultado = Valor1 / Valor2;
+        }
+
+        public void potencializar()
+        {
+            Resultado = Math.Pow(Valor1, Valor2);
+        }
+
+        public void radicionalizar()
+        {
+            Resultado = Math.Pow(Valor1, (1 / Valor2));
+        }
+
+        public void percentualizar()
+        {
+            Resultado = (Valor1 / Valor2) * 100;
+        }
+    }
+}
